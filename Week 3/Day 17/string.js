79d0aa6136f3e22442081cc7f8b8f262e6f8285e
Ali Naqi Khan <alinaqikhan10@gmail.com>
2026-08-17 23:14:27 +0500

    Day 17

---
+++ Week 3/Day 17/string.js
@@ -0,0 +1,6 @@
+let str = "I am Ali Naqi Khan. I am Software Engineering student. I am a teacher also and I love my students.";
+let words = str.split(" ");
+console.log(words);
+console.log(words.length);
+console.log(words.join(" "));
+console.log(str.replaceAll("am", "was"));
