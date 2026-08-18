3600474dc77fa9f7e4f7dcd2e914569e34769353
Ali Naqi Khan <alinaqikhan10@gmail.com>
2026-08-18 08:17:46 +0500

    Day 17

---
+++ Week 3/Day 17/stringMethods.js
@@ -0,0 +1,18 @@
+// let sentence = "Hello! How are you doing today? I hope you are having a great day.";
+// // Split method
+// let words = sentence.split(" ");
+// console.log(words);
+
+// // Join method
+// let programmingLanguages = ["JavaScript", "Python", "Java", "C++", "Ruby"];
+// console.log(programmingLanguages.join(" - "));
+
+// // Replace and replaceAll methods
+// console.log(sentence.replace("Hello", "Hi"));
+// console.log(sentence.replaceAll("day", "evening"));
+
+let text = "javascript is powerful and javascript is popular";
+let words = text.split(" ");
+console.log(words);
+console.log(text.replaceAll("javascript", "JavaScript"));
+console.log(words.join(" "));
